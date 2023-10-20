84e4aed1490c6fc7975669afe5a345481a23fc7c
SKKU-03\User <inhwancho02@skkukdp.re.kr>
2023-10-20 15:32:45 +0900

    rest api
    FBV - CRUD

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Students, Score
 
 # Register your models here.
+
+@admin.register(Students)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["name", "address", "email"]
+    list_display_links = ["name"]
+    
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ["Student", "english", "math", 'science', "date"]
+    list_display_links = ["Student"]
+    
